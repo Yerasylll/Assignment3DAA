b1d1232db96a4e2dd3b3bc98da3c805a77491040
Yerasyl Alimbek <erasylalimbek302@gmail.com>
2025-10-24 19:21:13 +0500

    Graph data structure and combined result

--- src/main/java/com/company/models/graphData/GraphData.java
+++ src/main/java/com/company/models/graphData/GraphData.java
@@ -1,4 +1,33 @@
 package com.company.models.graphData;
 
+import com.company.models.EdgeInput;
+
+import java.util.List;
+
 public class GraphData {
+    private int id;
+    private String description;
+    private List<String> nodes;
+    private List<EdgeInput> edges;
+
+    public GraphData(int id, String description, List<String> nodes, List<EdgeInput> edges) {
+        this.id = id;
+        this.description = description;
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public int getId() {return id;}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getNodes() {
+        return nodes;
+    }
+
+    public List<EdgeInput> getEdges() {
+        return edges;
+    }
 }
